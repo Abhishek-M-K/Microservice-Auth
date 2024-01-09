a5f563005e63bb3ad1fd7dba46c1727f0ccda30b
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-01-09 19:55:34 +0530

    feat (user) : add getUserByEmail

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const UserController = require("../../controllers/user-controller");
 
 router.post("/signup", UserController.create);
+router.get("/users", UserController.getByEmail);
 
 module.exports = router;
