c73843493802ed83a1d0e6da295801fbd0093c14
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-01-15 15:00:55 +0530

    feat (roles) : add userRole route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,5 +8,10 @@ router.post("/signup", AuthReqValidator.validateUser, UserController.create);
 router.post("/signin", AuthReqValidator.validateUser, UserController.signIn);
 router.get("/users", UserController.getByEmail);
 router.get("/isUserAuthenticated", UserController.isUserAuthenticated);
+router.get(
+  "/isAdmin",
+  AuthReqValidator.validateAdminReq,
+  UserController.isAdmin
+);
 
 module.exports = router;
